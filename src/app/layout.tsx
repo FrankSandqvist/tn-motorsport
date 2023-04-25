4f39d507e6fd0c5993156184deea12e4c6091a1e
Frank Sandqvist <kankki@gmail.com>
2023-04-25 15:07:16 +0300

    ...

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,8 +12,13 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.cdnfonts.com/css/monument-extended"
+        />
+      </head>
       <body className="bg-tyre text-white">{children}</body>
     </html>
-  )
+  );
 }
